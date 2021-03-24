93337efa9616bcd1b7f5826a258fb03ceb30fa2e
Łukasz Kiedrowski <lukaszkinder@gmail.com>
2021-03-24 14:16:10 +0100

    jest config updated

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  verbose: true,
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 };
