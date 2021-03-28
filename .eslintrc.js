81c965e043180ad1594127e9f38ea39622dce153
Łukasz Kiedrowski <lukaszkinder@gmail.com>
2021-03-29 00:48:00 +0200

    eslint prettier fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaOptions: 2018,
-    sourceType: 'module',
-  },
+	env: {
+		browser: false,
+		node: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
