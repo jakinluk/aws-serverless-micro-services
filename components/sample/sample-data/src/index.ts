e3984264379fe6d3855b3fe99a3c42078e5b9855
Łukasz Kiedrowski <lukaszkinder@gmail.com>
2021-04-10 18:33:30 +0200

    configuration base, polishing

--- components/sample/sample-data/src/index.ts
+++ components/sample/sample-data/src/index.ts
@@ -1,10 +1,24 @@
 import { Context } from 'aws-lambda/handler';
-import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda/trigger/api-gateway-proxy';
+import {
+	APIGatewayProxyEvent,
+	APIGatewayProxyHandler,
+	APIGatewayProxyResult,
+} from 'aws-lambda/trigger/api-gateway-proxy';
 import { GetDataItemService } from './sample/application/GetDataItemService';
+import { DynamoItemRepository } from './sample/application/repository/dynamo/DynamoItemRepository';
+import { ItemRepository } from './sample/application/repository/ItemRepository';
 import { GetDataItemGateway } from './sample/domain/GetDataItemGateway';
 import { GetDataItemHandlerConfiguration } from './sample/handler/GetDataItemHandlerConfiguration';
+import { config } from './sample/config/SampleDataConfiguration';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-const getDataItemGateway: GetDataItemGateway = new GetDataItemService(null);
-export const getDataItemHandler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context) => {
-	return GetDataItemHandlerConfiguration.getHandler(getDataItemGateway).handle(event, context);
+const dynamoDbDocumentClient: DocumentClient = new DocumentClient({ region: config.region });
+const itemrepository: ItemRepository = new DynamoItemRepository(dynamoDbDocumentClient, config.tableName);
+const getDataItemGateway: GetDataItemGateway = new GetDataItemService(itemrepository);
+
+export const getDataItemHandler: APIGatewayProxyHandler = async (
+	event: APIGatewayProxyEvent,
+	context: Context,
+): Promise<APIGatewayProxyResult> => {
+	return await GetDataItemHandlerConfiguration.getHandler(getDataItemGateway).handle(event, context);
 };
