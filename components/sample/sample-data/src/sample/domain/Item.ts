0d63f67c53e2cd3db9d1c2bb2821b99b77382911
Lukasz Kiedrowski <Lukasz.Kiedrowski@libertymutual.com>
2021-04-30 17:34:51 +0200

    polishing

--- components/sample/sample-data/src/sample/domain/Item.ts
+++ components/sample/sample-data/src/sample/domain/Item.ts
@@ -1,10 +1,22 @@
 import { Entity } from '@lkie/shared-model';
+import { EntityId } from '@lkie/shared-model';
 
 export interface ItemProps {
 	id: string;
 	prop1: string;
 	prop2: string;
+	createdAt: string;
+	updatedAt: string;
 }
 
 // Item props and Item (Entity)
-export class Item extends Entity<ItemProps> {}
+export class Item extends Entity<ItemProps> {
+	constructor(props: ItemProps ){
+		super(props, new EntityId(props.id));
+	}
+	update(newItem: Item):void {
+		this.props = newItem.getProps();
+		this.props.updatedAt = new Date().toISOString();
+	}
+	// static createFrom(command: CreateDataItemCommand)
+}
