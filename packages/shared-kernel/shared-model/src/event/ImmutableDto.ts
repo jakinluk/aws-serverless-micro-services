b9ac1c9457438b977a16fe29ef600a073e046a25
Łukasz Kiedrowski <lukaskiedrowski@gmail.com>
2023-07-09 20:15:14 +0200

    remove unnecessary dependency

--- packages/shared-kernel/shared-model/src/event/ImmutableDto.ts
+++ packages/shared-kernel/shared-model/src/event/ImmutableDto.ts
@@ -1,12 +1,11 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { freeze } from 'freeze-mutate';
 import { Dto } from './Dto';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export class ImmutableDto<T extends object> implements Dto<T> {
   protected readonly props: T;
   constructor(props: T) {
-  	this.props = freeze<T>(props);
+  	this.props = Object.freeze(props);
   }
   toJSON(): unknown {
   	return { ...this.props };
