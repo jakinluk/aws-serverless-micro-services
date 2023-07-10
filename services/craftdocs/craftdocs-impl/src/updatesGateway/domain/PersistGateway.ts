de1b9faad3f228dfe9099cd97219411c4a14272b
Łukasz Kiedrowski <lukaskiedrowski@gmail.com>
2023-07-10 22:21:47 +0200

    remove duplicates

--- services/craftdocs/craftdocs-impl/src/updatesGateway/domain/PersistGateway.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateDocLogCommand } from '@lkie/craftdocs-api';
-import { UpdateDocLogUseCaseError } from './UpdateDocLogUseCaseErrors';
-import { UseCaseVoidResult } from '@lkie/shared-model/src/use-case/UseCaseVoidResult';
-
-export interface PersistGateway {
-	persist(command: UpdateDocLogCommand): Promise<UseCaseVoidResult<UpdateDocLogUseCaseError>>;
-}
--- services/craftdocs/craftdocs-impl/src/updatesGateway/domain/UpdateDocLogUseCaseErrors.ts
+++
@@ -1,9 +0,0 @@
-import { UseCaseError } from '@lkie/shared-model';
-import { UpdateDocLogCommand } from '@lkie/craftdocs-api';
-
-export class UpdateDocLogUseCaseError extends UseCaseError {
-	constructor(command: UpdateDocLogCommand) {
-		super(`Unknown error occurred for command: ${JSON.stringify(command)}`);
-	}
-}
-
