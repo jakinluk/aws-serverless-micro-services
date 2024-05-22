1459d587e5794fb078d1a2e7c9395163ef7ef721
Łukasz Kiedrowski <lukaskiedrowski@gmail.com>
2024-05-22 12:10:45 +0200

    fix: correct base handler

--- packages/shared-kernel/shared-model/src/handler/BaseHandler.ts
+++ packages/shared-kernel/shared-model/src/handler/BaseHandler.ts
@@ -9,7 +9,9 @@ import { UseCaseVoidResult } from '../use-case/UseCaseVoidResult';
 export abstract class BaseHandler<I, E extends ImmutableDto<object>, U extends UseCase<E, UseCaseResult<object> | UseCaseVoidResult>> {
 	constructor(protected readonly useCase: U) {}
 	async handle(input: I, context: unknown): Promise<unknown> {
-		return await this.useCase.process(this.extractEvent(input, context));
+		return await this.process(this.extractEvent(input, context));
 	}
   	abstract extractEvent(input: I, context: unknown): E;
+	abstract process(event: E): Promise<any>;
+
 }
